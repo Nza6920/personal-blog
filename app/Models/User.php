00d665e47fa1f791f8ef35fa6565d3efb9b605c4
NiuZian <zian.niu@gdcccn.com>
2021-03-24 11:28:36 +0800

    修改样式

---
+++ app/Models/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+    use Traits\HashIdHelper;
+
+    protected $fillable = [
+        'name', 'email', 'password','avatar'
+    ];
+
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $dates = ['created_at', 'updated_at'];
+    
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
+}
